279641185897e7432a4d9499364060f7b55f2919
nio-p <nio.p.co.jp@gmail.com>
2018-01-27 19:44:17 +0900

    add setup script for osx

---
+++ setup_osx.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+set -e
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update
+
+brew install wget
+brew install git
+brew install tig
+brew install ag
+
+brew install rbenv
+brew install ruby-build
+
+#brew cask install atom
+#brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package quicklookase qlvideo
+
+wget --no-check-certificate http://install.ohmyz.sh -O - | sh
+chpass -s /bin/zsh
+
+DOT_FILES=".gitconfig .gitignore .vim .vimrc"
+
+for file in $DOT_FILES
+do
+  ln -s $HOME/dotfiles/$file $HOME
+done
+
+mkdir ~/.vim/bundle
+git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+git clone https://github.com/Shougo/vimproc ~/.vim/bundle/vimproc
+vim
