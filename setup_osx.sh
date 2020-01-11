9e96b42463c5b80f227d6910fd19a8af81c9a4f3
nio-p <nio.p.co.jp@gmail.com>
2020-01-11 17:39:06 +0900

    update

--- setup_osx.sh
+++ setup_osx.sh
@@ -13,8 +13,7 @@ brew install ag
 brew install rbenv
 brew install ruby-build
 
-#brew cask install atom
-#brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package quicklookase qlvideo
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package quicklookase qlvideo
 
 wget --no-check-certificate http://install.ohmyz.sh -O - | sh
 chpass -s /bin/zsh
