781a4f517c7aaf9c4839b56e36cb3c5d4bd7e9c6
nio-p <nio.p.co.jp@gmail.com>
2020-01-11 22:14:33 +0900

    update setup script

--- setup_osx.sh
+++ setup_osx.sh
@@ -2,22 +2,32 @@
 
 set -e
 
+# brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
-
 brew install wget
 brew install git
 brew install tig
 brew install ag
-
+brew install jq
 brew install awscli
 brew install terraform
-
 brew install rbenv
 brew install ruby-build
 
+# cask
 brew cask install qlcolorcode qlmarkdown quicklook-json quicklook-csv qlvideo
+brew cask install google-chrome
+brew cask install google-japanese-ime
+brew cask install slack
+brew cask install docker
 
+# dein
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+sh ./installer.sh ~/.cache/dein
+rm -f ./installer.sh
+
+# zsh
 wget --no-check-certificate http://install.ohmyz.sh -O - | sh
 chpass -s /bin/zsh
 mv ./.zshrc ~/.zshrc
@@ -29,7 +39,10 @@ do
   ln -s $HOME/dotfiles/$file $HOME
 done
 
-mkdir ~/.vim/bundle
-git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
-git clone https://github.com/Shougo/vimproc ~/.vim/bundle/vimproc
 vim
+
+# open vim and install dein
+# :call dein#install()
+
+# install node
+# https://github.com/nio-p/memo/blob/master/node.md
